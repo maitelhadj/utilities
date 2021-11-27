14f0405031c757f0a945fa95a666e2ebb5055653
mayasaeh <mayasaeh213@gmail.com>
2021-11-27 20:44:55 +0000

    Update docker.sh to install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# Install Docker and Docker Compose using the convenience script
+# Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+rm get-docker.sh
 sudo systemctl start docker
 sudo systemctl enable docker
+
+# Install Docker-Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
