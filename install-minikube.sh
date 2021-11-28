b2228a36f0bd555a63c50379b35ee8ed31a54b10
mayasaeh <mayasaeh213@gmail.com>
2021-11-28 17:56:58 +0000

    Adding install-minikube.sh

---
+++ install-minikube.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Clean
+rm minikube-linux-amd64
+
+# Start Minikube - Intall Kubernetes by the way
+minikube start
