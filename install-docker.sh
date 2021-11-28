0549fec35ace43ca744b914530c4d945705b8180
Mayas Ait Elhadj <mayasaeh213@gmail.com>
2021-11-28 12:51:50 +0100

    Update install-docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,6 +7,8 @@ rm get-docker.sh
 sudo systemctl start docker
 sudo systemctl enable docker
 
+sudo usermod -aG docker $(whoami) && newgrp docker
+
 # Install Docker-Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
